8cd63ca7639fe91415f0e4d0f7bd58dc698daac0
Grant Lemons <grantlemons@users.noreply.github.com>
2021-05-29 23:15:33 -0500

    tried stuff

--- main.ino
+++ main.ino
@@ -1,29 +1,73 @@
 #include <ESP8266WiFi.h>
-#include <ESP8266AWSImplementations.h>
-#include <AmazonSNSClient.h>
+#include <MqttClient.h>
+#include <ArduinoMqttClient.h>
+#include <ArduinoJson.h>
+#include <Wire.h>
+#include <string>
 
 #include "secrets.h"
 
-#define topic_name "arn:aws:sns:us-west-2:646796572767:zombie-hyphen-man.fifo"
-String wifi_address;
+#define thing_name "PoolPump"
+#define endpoint "a232amt01mo7lf-ats.iot.us-west-2.amazonaws.com"
 
-Esp8266HttpClient httpClient;
-Esp8266DateTimeProvider dateTimeProvider;
+ESP8266WiFiClass net = ESP8266WiFi();
+MqttClient client = MqttClient(256);
 
-AmazonSNSClient snsClient;
-ActionError actionError;
+String lastValue;
 
 void setup() {
     WiFi.begin( wifi_ssid, wifi_password );
     while ( WiFi.status() != WL_CONNECTED ) {}
-    wifi_address = WiFi.localIP();
-    snsClient.setAWSRegion("us-west-2");
-    snsClient.setAWSEndpoint("amazonaws.com");
-    snsClient.setAWSPath("/sns/v3/home?region=us-west-2#/dashboard");
-    snsClient.setAWSKeyID(access_id);
-    snsClient.setAWSSecretKey(secret_key);
-    snsClient.setHttpClient(&httpClient);
-    snsClient.setDateTimeProvider(&dateTimeProvider);
+}
+
+void activate( string input ) {
+    switch ( input ) {
+        case "1":
+            // Power 1
+            lastValue = input;
+            break;
+        case "2":
+            // Power 2
+            lastValue = input;
+            break;
+        case "3":
+            // Power 3
+            lastValue = input;
+            break;
+        case "4":
+            // Power 4
+            lastValue = input;
+            break;
+        case "5":
+            // Power 5
+            lastValue = input;
+            break;
+        case "6":
+            // Power 6
+            lastValue = input;
+            break;
+        case "7":
+            // Power 7
+            lastValue = input;
+            break;
+        case "8":
+            // Power 8
+            lastValue = input;
+            break;
+
+
+        case "ON":
+            if ( lastValue != "" ) { activate( lastValue ); }
+            break;
+        case "OFF":
+            // Off
+            break;
+
+
+        default:
+            // Nothing?
+            break;
+    }
 }
 
 void loop() {
